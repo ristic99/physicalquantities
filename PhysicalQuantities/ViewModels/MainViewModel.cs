eaaca1e3cb0b31630473adedf2052ef7f19b28fa
Mirijan Ristic <ristic.mirijan@gmail.com>
2025-07-25 14:09:35 +0200

    cleanup: remove redundant System.Windows import from MainViewModel

--- PhysicalQuantities/ViewModels/MainViewModel.cs
+++ PhysicalQuantities/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using PhysicalQuantities.Core.Enums;
 using PhysicalQuantities.Core.Physics;
