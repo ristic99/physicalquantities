a22a0b5b430abdfc5b02460200a1690c503a3013
Mirijan Ristic <ristic.mirijan@gmail.com>
2025-10-30 22:27:44 +0100

    Add files

---
+++ PhysicalQuantities/Core/Enums/OperationType.cs
@@ -0,0 +1,9 @@
+﻿namespace PhysicalQuantities.Core.Enums;
+
+public enum OperationType
+{
+    Direct,        // Created directly by user
+    ScalarMultiply,// Scalar * Scalar or Scalar * Vector
+    DotProduct,    // Vector · Vector → Scalar
+    CrossProduct   // Vector × Vector → Pseudovector
+}
---
+++ PhysicalQuantities/Core/Enums/QuantityNature.cs
@@ -0,0 +1,8 @@
+﻿namespace PhysicalQuantities.Core.Enums;
+
+public enum QuantityNature
+{
+    Scalar,       // Energy, Mass, Time, Temperature
+    Vector,       // Force, Velocity, Displacement  
+    Pseudovector  // Torque, Angular Momentum, Angular Velocity
+}
